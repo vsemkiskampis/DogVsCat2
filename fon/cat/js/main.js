0ddc9860a682c9011d6395eb7d827402ce9c45d3
vsemkiskampis <valera_maslov_95@mail.ru>
2021-06-22 15:40:09 +0700

    123

---
+++ fon/cat/js/main.js
@@ -0,0 +1,97 @@
+window.onload = function(){
+var area = document.getElementById('area');
+var cell = document.getElementsByClassName('cell');
+var currentPlayer = document.getElementById('curPlyr');
+let img = document.getElementsByClassName('img');
+var player = "Cat";
+var stat = {
+    "Corgi": 0,
+    "Cat": 0,
+    'd': 0
+}
+var winIndex = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7]
+];
+
+
+
+for (var i = 0; i< cell.length; i++) {
+    cell[i].addEventListener('click', cellClick, false);
+}
+
+function cellClick() {
+
+    var data = [];
+    
+    if(!this.innerHTML) {
+        this.innerHTML = player;
+    }else {
+        alert("Ячейка занята");
+        return;
+    }
+
+    for(var i in cell){
+        if(cell[i].innerHTML == player){
+            data.push(parseInt(cell[i].getAttribute('pos')));
+        }
+    }
+
+    if(checkWin(data)) {
+        stat[player] += 1;
+        restart("Выграл: " + player);
+        
+    }else {
+        var draw = true;
+        for(var i in cell) {
+            if(cell[i].innerHTML == '') draw = false;
+        }
+        if(draw) {
+            stat.d += 1;
+            restart("Ничья");
+        }
+    }
+    player = player == "Cat" ? "Corgi" : "Cat";
+   
+    currentPlayer.innerHTML = player.toUpperCase();
+}
+
+function checkWin(data) {
+    for(var i in winIndex) {
+        var win = true;
+        for(var j in winIndex[i]) {
+            var id = winIndex[i][j];
+            var ind = data.indexOf(id);
+
+            if(ind == -1) {
+                win = false
+            }
+        }
+
+        if(win) return true;
+    }
+    return false;
+}
+
+function restart(text) {
+    
+    alert(text);
+    for(var i = 0; i < cell.length; i++) {
+        cell[i].innerHTML = '';
+    }
+    updateStat();
+}
+
+function updateStat() {
+    document.getElementById('sX').innerHTML = stat.Corgi;
+    document.getElementById('sO').innerHTML = stat.Cat;
+    document.getElementById('sD').innerHTML = stat.d;
+}
+
+}
