8ffbcb597f2089e5413948371badd9eaa27dd2a4
vsemkiskampis <valera_maslov_95@mail.ru>
2021-06-23 23:29:28 +0700

    qqqq

--- fon/cat/js/js2.js
+++ fon/cat/js/js2.js
@@ -30,3 +30,11 @@ document.addEventListener('mousemove',y1);
 
 
 
+function myFunction() {
+   var x = document.getElementById("myLinks");
+   if (x.style.display === "block") {
+     x.style.display = "none";
+   } else {
+     x.style.display = "block";
+   }
+ }
--- fon/dog/js/js.js
+++ fon/dog/js/js.js
@@ -32,3 +32,12 @@ document.addEventListener('mousemove',y1);
 
 
 
+
+    function myFunction() {
+      var x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
